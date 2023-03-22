98da81ba4ce72fc295d8036eaad52d25d5f7ad61
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-03-22 20:35:26 +0530

    Add files via upload

---
+++ contests/Starters 82/Candy_Division.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+		cin>>x;
+		if(x%3==0)cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+	}
+	return 0;
+}
