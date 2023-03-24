a71fa90186b2ba10acf4d5dbd08b7953b06dc18b
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-03-24 22:35:11 +0530

    Add files via upload

---
+++ Diff 701 to 850/Cyclic_Quadrilateral.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c,d;
+		cin>>a>>b>>c>>d;
+		if((a+c)==180 && (b+d)==180)cout<<"YES"<<endl;
+		else if((a+c)!=180 || (b+d)!=180)cout<<"NO"<<endl;
+	}
+	return 0;
+}
