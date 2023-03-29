b3106b13c35b1d40beab12646b851fe4b2c2505f
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-03-29 21:42:24 +0530

    Add files via upload

---
+++ contests/Starters 83/Budget_of_Technex.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+		cin>>x;
+        x *= 1000;
+        cout<<(x/2)/5<<endl;
+		
+	}
+	return 0;
+}
