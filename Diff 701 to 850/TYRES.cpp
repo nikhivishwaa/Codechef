edfd4617a8978a64b3d573df4f8462fb5b7a37e0
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-04-25 23:06:52 +0530

    Add files via upload

---
+++ Diff 701 to 850/TYRES.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;cin>>t;
+	while(t--)
+	{int tyres;
+	cin>>tyres;
+	if(tyres%4>=2)
+	cout<<"YES"<<endl;
+	else
+	cout<<"NO"<<endl;
+	
+	}
+	return 0;
+}
