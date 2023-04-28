493a2246c8217affce4a62c2ed94afb3e47f1743
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-04-28 20:29:18 +0530

    Add files via upload

---
+++ Diff. 200 to 557/Four_Tickets.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+		cin>>x;
+        if(x*4<=1000)cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+		
+	}
+	return 0;
+}
