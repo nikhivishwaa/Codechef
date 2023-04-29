cbbc1553e57ca12a378484d7f59aae2939260870
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-04-29 21:32:14 +0530

    Add files via upload

---
+++ Diff 558 to 700/Buy_Ten_Packets.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    cout<<((2*y)+(x*1))<<endl;
+	}
+	return 0;
+}
