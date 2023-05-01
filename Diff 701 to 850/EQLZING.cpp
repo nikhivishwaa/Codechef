9b34fa36c452ccc2495cb2d71eba366ea8fd46ed
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-05-01 23:17:21 +0530

    Add files via upload

---
+++ Diff 701 to 850/EQLZING.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+	    int a, b;
+	    cin >> a >> b;
+	    if (abs(a - b) % 2 != 0) {
+	        cout << "No" << endl;
+	    } else {
+	        cout << "Yes" << endl;
+	    }
+	}
+	return 0;
+}
